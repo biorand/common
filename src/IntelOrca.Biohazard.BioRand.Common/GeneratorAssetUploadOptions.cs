8f95d940cb0cf43377c9af61d0b0701cf6e700d0
Ted John <tjohn@rocketsoftware.com>
2026-05-28 16:46:26 +0100

    Implement chunked asset API

---
+++ src/IntelOrca.Biohazard.BioRand.Common/GeneratorAssetUploadOptions.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace IntelOrca.Biohazard.BioRand
+{
+    public sealed class GeneratorAssetUploadOptions
+    {
+        public const int DefaultSmallAssetThresholdBytes = 32 * 1024 * 1024;
+        public const int DefaultChunkSizeBytes = 8 * 1024 * 1024;
+        public const int DefaultMaxParallelUploads = 4;
+        public const int DefaultMaxChunkAttempts = 5;
+        public const int DefaultMaxConsecutiveChunkFailures = 5;
+
+        public int SmallAssetThresholdBytes { get; set; } = DefaultSmallAssetThresholdBytes;
+        public int ChunkSizeBytes { get; set; } = DefaultChunkSizeBytes;
+        public int MaxParallelUploads { get; set; } = DefaultMaxParallelUploads;
+        public int MaxChunkAttempts { get; set; } = DefaultMaxChunkAttempts;
+        public int MaxConsecutiveChunkFailures { get; set; } = DefaultMaxConsecutiveChunkFailures;
+
+        internal void Validate()
+        {
+            if (SmallAssetThresholdBytes < 0)
+                throw new ArgumentOutOfRangeException(nameof(SmallAssetThresholdBytes));
+            if (ChunkSizeBytes <= 0)
+                throw new ArgumentOutOfRangeException(nameof(ChunkSizeBytes));
+            if (MaxParallelUploads <= 0)
+                throw new ArgumentOutOfRangeException(nameof(MaxParallelUploads));
+            if (MaxChunkAttempts <= 0)
+                throw new ArgumentOutOfRangeException(nameof(MaxChunkAttempts));
+            if (MaxConsecutiveChunkFailures <= 0)
+                throw new ArgumentOutOfRangeException(nameof(MaxConsecutiveChunkFailures));
+        }
+    }
+}
